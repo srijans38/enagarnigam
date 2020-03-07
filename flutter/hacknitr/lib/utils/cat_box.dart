50b43048ed2634545eb906b652984b4c6855a8ea
Srijan Sharma <srijans38@gmail.com>
2020-03-07 21:23:38 +0530

    waves

--- flutter/hacknitr/lib/utils/cat_box.dart
+++ flutter/hacknitr/lib/utils/cat_box.dart
@@ -5,13 +5,15 @@ class CatBox extends StatelessWidget {
   final String icon;
   final String label;
   final double height;
+  final int wave;
+  final double top;
 
-  CatBox({this.color, this.icon, this.label, this.height});
+  CatBox({this.color, this.icon, this.label, this.height, this.wave, this.top});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(8.0),
+      //padding: EdgeInsets.all(8.0),
       margin: EdgeInsets.only(right: 20.0),
       decoration: BoxDecoration(
         color: color,
@@ -21,20 +23,36 @@ class CatBox extends StatelessWidget {
         minWidth: MediaQuery.of(context).size.width / 2.75,
         maxWidth: MediaQuery.of(context).size.width / 2.75,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Stack(
         children: <Widget>[
-          Image.asset(
-            icon,
-            height: height ?? MediaQuery.of(context).size.height / 10,
+          // Positioned(
+          //   top: top,
+          //   right: 0.0,
+          //   child: Image.asset(
+          //     'assets/intersect$wave.png',
+          //     height: MediaQuery.of(context).size.height / 4,
+          //     width: MediaQuery.of(context).size.width / 2.75,
+          //     alignment: Alignment.centerRight,
+          //   ),
+          // ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset(
+                  icon,
+                  height: height ?? MediaQuery.of(context).size.height / 10,
+                ),
+                Text(
+                  label,
+                  style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 22.5,
+                      fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
           ),
-          Text(
-            label,
-            style: TextStyle(
-                fontFamily: 'Raleway',
-                fontSize: 22.5,
-                fontWeight: FontWeight.w500),
-          )
         ],
       ),
     );
