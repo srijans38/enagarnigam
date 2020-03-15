6edf5e456e23ed9fcd6e3c29f15ac58d7d530f93
ssal-man <ssalmanyo9@gmail.com>
2020-03-16 02:47:56 +0530

    CSS

--- DJeNagarNigam/mainapp/urls.py
+++ DJeNagarNigam/mainapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
         path('', RedirectView.as_view(url='home/')),
-        path('home/',views.homeview,name='home')
+        path('home/',views.homeview,name='home'),
+        path('services/',views.srview,name='sr'),
+        path('reports/',views.reportview,name='report'),
+        path('forum/',views.forumview,name='forum'),
+        path('map/',views.mapview,name='map'),
 ]
--- DJeNagarNigam/mainapp/views.py
+++ DJeNagarNigam/mainapp/views.py
@@ -7,3 +7,15 @@ from django.shortcuts import render
 
 def homeview(request):
     return render(request,'mainapp/home.html')
+
+def forumview(request):
+    return render(request,"mainapp/forum.html")
+
+def reportview(request):
+    return render(request,"mainapp/report.html")
+
+def mapview(request):
+    return render(request,"mainapp/map.html")
+
+def srview(request):
+    return render(request,"mainapp/sr.html")               
