ae42c3af84f381bc20bb3efacc5111c0d9bc8987
ssal-man <ssalmanyo9@gmail.com>
2020-03-17 01:03:27 +0530

    firebase config

---
+++ DJeNagarNigam/static/mainapp/js/firebaseconfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQoHFB5qf0ufrDDRjTcwAHfjfqwdV__vY",
+    authDomain: "hacknitr-624ce.firebaseapp.com",
+    databaseURL: "https://hacknitr-624ce.firebaseio.com",
+    projectId: "hacknitr-624ce",
+    storageBucket: "hacknitr-624ce.appspot.com",
+    messagingSenderId: "768745989208",
+    appId: "1:768745989208:web:5974cf97836a5f752cd62d",
+    measurementId: "G-760592MS4P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db=firebase.firestore();
